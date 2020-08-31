7d86fd830e508875a4029b95f4aaa91a6955f7f4
Holly <61398991+hmilly@users.noreply.github.com>
2020-08-31 21:27:38 +0100

    Working through this currently.Not all of it works

--- 53-http-server/index.js
+++ 53-http-server/index.js
@@ -17,3 +17,58 @@ const users = [
   },
 ];
 
+
+
+
+
+
+const http = require('http')
+const fs = require('fs')
+
+const handleRequest = (request, response) => {
+
+
+
+
+
+
+
+
+
+  if (request.url === '/') {
+    return response.end('<html><p>Hello</></p></html>')
+  }
+
+  if (request.url === '/home') {
+    fs.readFileSync('./index.html', 'utf8', (error, data) => {
+      if (error) {
+        response.end('oops')
+      } else {
+        response.end(data)
+      }
+    })
+  }
+  
+  if (request.url === '/api/users') {
+    return response.end(`
+    <ul>
+    ${users.map(u =>
+      `<li>${u}</li>`
+    )}
+    </ul>
+    `)
+  }
+  
+  if (request.url === '/') {
+    return response.end('<html><p>Hello</></p></html>')
+  }
+  
+  response.end('<html><h2>Error</h2></html>')
+}
+
+
+const server = http.createServer(handleRequest)
+
+server.listen(2000);
+
+
