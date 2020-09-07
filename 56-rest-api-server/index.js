570213298fe45b524669f2a852b9ba621f48c96c
Holly <milstowers@gmail.com>
2020-09-07 19:49:51 +0100

    working through 56

--- 56-rest-api-server/index.js
+++ 56-rest-api-server/index.js
@@ -1,128 +1,182 @@
 const express = require("express")
-const fs = require("fs")
+const fs = require("fs");
+const { query } = require("express");
 
 const app = express();
 app.use(express.json());
 
-app.get("/users", async (req, res) => {
 
-    const userFile = ".db/users.json";
-    const users = JSON.parse(fs.readFileSync(userFile, "utf-8"))
+
+// Endpoint - /users/
+
+// GET - Returns all users as a JSON object.
+
+app.get("/users", (req, res) => {
+    const users = JSON.parse(fs.readFileSync(`./db/users.json`, "utf-8"))
+    res.setHeader("Content-type", "application/json")
+    res.status(200).send( users );
+    return
+})
+
+
+// POST - adds a new user. As a response returns new user.
+
+app.post("/users", async(req, res) => {
+    let users = JSON.parse(fs.readFileSync(`./db/users.json`, "utf-8"))
+
+    try {
+    users = [...users, {...req.body}]
+    fs.writeFileSync(`./db/users.json`, JSON.stringify(users))  
+    }
+    catch (error) {
+        console.log(error)
+    }
 
     res.setHeader("Content-type", "application/json")
-    res.status(200).send({ result: users });
+    res.status(200).send( req.body );
     return
 })
 
 
 
-// app.post("/users", async (req, res) => {
+// Endpoint - /user/:id
 
-//     const userFile = ".db/users.json";
-//     const users = JSON.parse(fs.readFileSync(userFile, "utf-8"))
-    
-
-//     res.setHeader("Content-type", "application/json")
-//     res.status(200).send({ result: users });
-//     return
-// })
-
-
-
-
-app.get("/user/id", async (req, res) => {
-
-    const u = ".db/users/id.json";
-    const user = JSON.parse(fs.readFileSync(u, "utf-8"))
-
+// GET - Returns a user.
+app.get("/user/:id", (req, res) => {
+    const addr = `./db/users.json`
+    let user = JSON.parse(fs.readFileSync(addr, "utf-8"))
+    let us = user.find(u => u.id === req.param.id)
+    console.log("user", )
     res.setHeader("Content-type", "application/json")
-    res.status(200).send({ result: user });
+    res.status(200).send( user );
     return
 })
 
-// app.patch("/user/id", async (req, res) => {
+// PATCH - updates an existing user by id. As a response returns new user.
 
-//     const u = ".db/users/id.json";
-//     const user = JSON.parse(fs.readFileSync(u, "utf-8"))
-
-//     res.setHeader("Content-type", "application/json")
-//     res.status(200).send({ result: user });
-//     return
-// })
-
-// app.delete("/user/id", async (req, res) => {
-
-//     const u = ".db/users/id.json";
-
-
-//     res.setHeader("Content-type", "application/json")
-//     res.status(200).send({ result: user.id });
-//     return
-// })
-
-
-
-
-
-app.get("/orders", async (req, res) => {
-
-    const o = ".db/orders.json";
-    const orders = JSON.parse(fs.readFileSync(o, "utf-8"))
-
+app.patch("/user/:id", (req, res) => {
+    const addr = `./db/users.json`;
+    let user = JSON.parse(fs.readFileSync(addr, "utf-8"))
+    user = {
+        id: 6,
+        firstName: "owen",
+        lastName: "pring",
+        email: "op@gmial.com"
+    }
+    fs.writeFileSync(addr, JSON.stringify(user))
     res.setHeader("Content-type", "application/json")
-    res.status(200).send({ result: orders });
+    res.status(200).send(user );
     return
 })
 
-// app.post("/orders", async (req, res) => {
+// DELETE - removes an existing user by id. As a response returns id of removed user.
 
-//     const o = ".db/orders.json";
-//     const orders = JSON.parse(fs.readFileSync(o, "utf-8"))
+app.delete("/user/:id", (req, res) => {
 
-//     res.setHeader("Content-type", "application/json")
-//     res.status(200).send({ result: orders });
-//     return
-// })
+    const user = `./db/orders.json`;
 
-
-
-// app.get("/orders?userId=:id", async (req, res)=> {
-
-//     const o = ".db/orders.json";
-//     const orders = JSON.parse(fs.readFileSync(o, "utf-8"))
-
-//     res.setHeader("Content-type", "application/json")
-//     res.status(200).send({ result: orders });
-//     return
-// })
+    res.setHeader("Content-type", "application/json")
+    res.status(200).send(req.params.id);
+    return
+})
 
 
-// app.get("/order:id", async (req, res)=> {
+// Endpoint - /orders
 
-//     const o = ".db/orders/id.json";
-//     const orders = JSON.parse(fs.readFileSync(o, "utf-8"))
+// GET - Returns all orders.
 
-//     res.setHeader("Content-type", "application/json")
-//     res.status(200).send({ result: orders });
-//     return
-// })
+app.get("/orders", (req, res) => {
+    const orders = fs.readFileSync("./db/orders.json", "utf-8")
 
-// app.patch("/order:id", async (req, res)=> {
+    res.setHeader("Content-type", "application/json")
+    res.status(200).send( JSON.parse(orders) );
+    return
+})
 
-//     const o = ".db/orders/id.json";
-//     const orders = JSON.parse(fs.readFileSync(o, "utf-8"))
+// POST - adds a new order. As a response returns a new order.
 
-//     res.setHeader("Content-type", "application/json")
-//     res.status(200).send({ result: orders });
-//     return
-// })
+app.post("/orders", (req, res) => {
+    const orders = JSON.parse(fs.readFileSync("./db/orders.json", "utf-8"))
+    const newOrder = {
+        id: 1,
+        userId: 4,
+        productName: "orange",
+        quantity: 2
+    }
+    orders = [...orders, newOrder]
+    fs.writeFileSync("./db/orders.json", JSON.stringify(orders))
+    res.setHeader("Content-type", "application/json")
+    res.status(200).send( newOrder );
+    return
+})
 
-// app.delete("/orders:id, async (req, res)=> {
 
-//     const o = ".db/orders/id.json";
-//     const orders = JSON.parse(fs.readFileSync(o, "utf-8"))
 
-//     res.setHeader("Content-type", "application/json")
-//     res.status(200).send({ result: orders });
-//     return
-// })
+// Endpoint - /orders?userId=:id
+
+// GET - Returns all orders by user id.
+
+app.get("/orders?userId=:id", (req, res) => {
+
+    const oAddr = `./db/orders.json`;
+    const orders = JSON.parse(fs.readFileSync(oAddr, "utf-8"))
+
+    res.setHeader("Content-type", "application/json")
+    res.status(200).send( orders );
+    return
+})
+
+
+
+
+// Endpoint - /order/:id
+
+// GET - Returns an order.
+
+app.get("/order:id", (req, res) => {
+    const oAddr = `./db/orders.json`;
+    const order = fs.readFileSync(oAddr, "utf-8")
+
+    res.setHeader("Content-type", "application/json")
+    res.status(200).send( JSON.parse(order ));
+    return
+})
+
+// PATCH - updates an existing order by id. As a response returns modified order.
+
+app.patch("/order:id", (req, res) => {
+
+    const oAddr = `./db/orders.json`;
+    let order = JSON.parse(fs.readFileSync(oAddr, "utf-8"))
+    order = {
+        id: 1,
+        userId: 4,
+        productName: "orange",
+        quantity: 2
+    }
+   fs.writeFileSync(oAddr, JSON.stringify(order))
+    res.setHeader("Content-type", "application/json")
+    res.status(200).send(order );
+    return
+})
+
+
+// DELETE - removes an existing order by id. As a response returns id of deleted order.
+
+app.delete("/orders:id", (req, res) => {
+
+    const oAddr = `./db/orders.json`;
+    res.setHeader("Content-type", "application/json")
+    res.status(200).send(req.params.id);
+    return
+})
+
+app.listen(3001)
+
+
+// router.delete('/:id', function (req, res) {
+//     User.findByIdAndRemove(req.params.id, function (err, user) {
+//       if (err) return res.status(500).send("There was a problem deleting the user.");
+//       res.status(200).send("User: "+ user.name +" was deleted.");
+//     });
+//   });
