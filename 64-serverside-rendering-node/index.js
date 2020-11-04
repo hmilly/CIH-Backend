b82a2aa140bcd75344319f9804ebd8903286f7d2
Holly <milstowers@gmail.com>
2020-11-04 20:36:50 +0000

    working on 64

--- 64-serverside-rendering-node/index.js
+++ 64-serverside-rendering-node/index.js
@@ -1,13 +1,17 @@
 const express = require("express")
 const fs = require("fs");
+const db = require('./db')
+const fetch = require("node-fetch");
 
 const app = express();
 app.use(express.json());
 
 
 app.get("/", (req, res) => {
-    const users = JSON.parse(fs.readFileSync('./db.json', "utf-8"))
+    db.getAllPokemon().then(pokemons => {
 
+        res.send(pokemons)
+    })
 })
 
 app.get("/:userId", (req, res) => {
