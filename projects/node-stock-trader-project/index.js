89413fc2bc6098232734ac737f8ac57da0b5f3c0
Holly <milstowers@gmail.com>
2020-12-23 17:27:52 +0000

    updated some errors, changed html, changed stock

--- projects/node-stock-trader-project/index.js
+++ projects/node-stock-trader-project/index.js
@@ -17,40 +17,71 @@ app.get("/", (req, res) => {
 });
 
 var requestOptions = {
-  'url': `https://api.tiingo.com/iex/?tickers=spy,tsla,amzn,wmt,dis&token=${token}`,
+  'url': `https://api.tiingo.com/iex/?tickers=sne,tsla,amzn,wmt,dis&token=${token}`,
   'headers': {
     'Content-Type': 'application/json'
   }
 };
 
+let obj = [
+  {
+    id: 1,
+    stockName: "Amazon",
+    tag: "AMZN",
+    currentPrice: 0
+  },
+  {
+    id: 2,
+    stockName: "Disney",
+    tag: "DIS",
+    currentPrice: 0
+  },
+  {
+    id: 3,
+    stockName: "Sony",
+    tag: "SNE",
+    currentPrice: 0
+  },
+  {
+    id: 4,
+    stockName: "Tesla",
+    tag: "TSLA",
+    currentPrice: 0
+  },
+  {
+    id: 5,
+    stockName: "Walmart",
+    tag: "WMT",
+    currentPrice: 0
+  }
+]
+
 const int = () => {
   request(requestOptions,
     function (error, response, body) {
-      let obj = []
-      JSON.parse(body).map((item, i) => {
+      console.log(body)
+      JSON.parse(body).map(item => {
+        console.log(item)
+        const findPrice = (price) =>
+          obj.map(i => {
+            if (i.tag === item.ticker)
+              i.currentPrice = price
+          })
+
         if (item.bidPrice) {
-          obj.push({
-            "id": i + 1,
-            "stockName": item.ticker,
-            "currentPrice": item.bidPrice
-          })
+          findPrice(item.bidPrice)
         } else {
-          obj.push({
-            "id": i + 1,
-            "stockName": item.ticker,
-            "lastPrice": item.prevClose
-          })
-        clearInterval(int)
+          findPrice(item.prevClose)
+          clearInterval(interval)
+          window.alert("Markets closed for today! Last prices are below :)")
         }
       })
       io.on("connection", (socket) => {
-        console.log("running!")
       })
+      console.log(obj)
       io.sockets.emit("updateStock", obj)
     })
 }
-
-
 
 io.on("connection", (socket) => {
   int()
@@ -58,10 +89,9 @@ io.on("connection", (socket) => {
   io.emit("printUsers", users)
 })
 
-// const interval = setInterval(() => {
-//   int()
-// }, 5000);
-
-
+const interval = setInterval(() => {
+  int()
+}, 5000);
 
 server.listen(3000, () => console.log("project listening!"))
+process.setMaxListeners(0)
--- projects/node-stock-trader-project/public/indexPage.js
+++ projects/node-stock-trader-project/public/indexPage.js
@@ -1,33 +1,70 @@
 const socket = io();
-const select = (item) => document.querySelector(`.${item}`)
+const select = (item) => this.document.querySelector(`.${item}`)
 const stockMain = select("stock-main")
 const usersMain = select("users-main")
 
 
+
+
 socket.on("updateStock", (stockData) => {
+    //console.log(stockData)
     stockData.map((stock, i) => {
         const stockNum = select(`stock${i + 1}`)
+        
         stockNum.innerText =
             `${stock.currentPrice ? stock.currentPrice : stock.lastPrice}`
     })
 })
 
+let currentUser
 socket.once("printUsers", (userData) => {
-
     userData.map((user, i) => {
         select("users-main").innerHTML +=
-            `<div class="user user${i}">
+            `<div class="user">
         <h3 class="username">${user.firstName} ${user.lastName}</h3>
         <p class="user-balance">${user.balance}</p>
-        <div class="users-shares"></div>
+        <div class="users-shares us${i}"></div>
         <button class="user-btn">Select user</button>
     </div>`
-        userData[i].transactions.map((data, j) => {
-            select("users-shares").innerHTML +=
+        userData[i].transactions.map(data => {
+            select(`us${i}`).innerHTML +=
                 `<div class="shares-div">
             <h3 class="company-name">${data.shareName}</h3>
             <h5 class="quantity">${data.quantity}</h5>
             </div>`
+        })
+    })
+
+    const userbutton = document.querySelectorAll(".user-btn")
+    userbutton.forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            // removes prev user
+            userbutton.forEach(b => {
+                b.parentElement.classList.remove("active")
+                b.innerText = "Select user"
+            })
+            const userDiv = e.path[1]
+
+            //sets current user
+            userDiv.classList.add('active')
+            btn.innerText = "Current User"
+
+            let shareObj = []
+            const userShares = userDiv.querySelectorAll(".shares-div")
+            userShares.forEach(s => {
+                const companyName = s.querySelector(".company-name").innerText
+                const quantity = s.querySelector(".quantity").innerText
+                shareObj.push({ companyName, quantity })
+            })
+
+            currentUser = {
+                userDiv: userDiv,
+                name: userDiv.querySelector(".username").innerText,
+                balance: parseInt(userDiv.querySelector(".user-balance").innerText),
+                shares: shareObj
+            }
         })
     })
 })
@@ -47,14 +84,37 @@ minusBtns.forEach(btn => btn.addEventListener("click", (e) => {
 }))
 
 
+
+
+
+
 const buyBtns = document.querySelectorAll(".buy-btn")
 buyBtns.forEach(btn => btn.addEventListener("click", (e) => {
     e.preventDefault();
+    const company = btn.parentElement.querySelector(".stock-name").innerText
     const inputVal = e.path[1].querySelector(".quantity").value
-    if (inputVal > 0) {
-        e.path[1].querySelector(".quantity").value = 0
-    }
+
+
+
+    const currentPrice = e.path[1].querySelector(".stock-price").innerText
+    console.log(currentPrice)
+    // currentprice.split(".")
+    // curentprice[0]
+
+
+
+console.log(currentPrice)
+if (inputVal > 0) {
+    e.path[1].querySelector(".quantity").value = 0
+    const buyInput = { company, inputVal }
+    console.log(currentUser, buyInput)
+}
 }))
+
+
+
+
+
 
 const sellBtn = document.querySelectorAll(".sell-btn")
 sellBtn.forEach(btn => btn.addEventListener("click", (e) => {
@@ -64,36 +124,3 @@ sellBtn.forEach(btn => btn.addEventListener("click", (e) => {
         e.path[1].querySelector(".quantity").value = 0
     }
 }))
-
-
-let currentUser
-
-
-setTimeout(() => {
-    const userbutton = document.querySelectorAll(".user-btn")
-
-    userbutton.forEach(btn => btn.addEventListener("click", (e) => {
-        e.preventDefault();
-        const userDiv = e.path[1]
-        let shareObj = []
-        const userShares = userDiv.querySelectorAll(".shares-div")
-        userShares.forEach(s => {
-            const companyName = s.querySelector(".company-name").innerText
-            const quantity = s.querySelector(".quantity").innerText
-            shareObj.push({ companyName, quantity })
-        })
-
-        const userName = userDiv.querySelector(".username").innerText
-        const userBalance = parseInt(userDiv.querySelector(".user-balance").innerText)
-        currentUser = { userName: userName, userBalance: userBalance, userShares: shareObj }
-
-        userDiv.classList.add('active')
-        btn.innerText = "Current User"
-
-    }))
-
-
-}, 500)
-
-
-
